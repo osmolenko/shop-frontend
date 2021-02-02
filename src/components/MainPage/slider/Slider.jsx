91f784f0f6887e51d349ff5e9f4306280b52e43a
osmolenko <57684564+osmolenko@users.noreply.github.com>
2021-02-02 17:40:22 +0200

    fix: change react-simple-slider to swiper

--- src/components/MainPage/slider/Slider.jsx
+++ src/components/MainPage/slider/Slider.jsx
@@ -1,29 +1,28 @@
 import React from 'react'
-import SimpleImageSlider from "react-simple-image-slider";
-import {SliderContainer} from "./Slider.styled";
+import {Swiper, SwiperSlide} from "swiper/react";
+import slidero from "../../../images/banner/1.png"
+import slidert from "../../../images/banner/2.png"
+import sliderh from "../../../images/banner/3.png"
+import sliderf from "../../../images/banner/4.png"
+import SwiperCore, {Navigation, Pagination, A11y, Autoplay} from "swiper";
 
 export default class Slider extends React.Component{
     render(){
-
-        const images = [
-            { url: "https://shop.osmolenko.cloud/banner/1.png" },
-            { url: "https://shop.osmolenko.cloud/banner/2.png" },
-            { url: "https://shop.osmolenko.cloud/banner/3.png" },
-            { url: "https://shop.osmolenko.cloud/banner/4.png" },
-        ];
-
+        SwiperCore.use([Navigation, Pagination, Autoplay, A11y]);
         return(
-            <SliderContainer>
-                <SimpleImageSlider
-                    width="100%"
-                    height="80vh"
-                    images={images}
-                    useGPURender
-                    showBullets
-                    showNavs
-                    navStyle='2'
-                />
-            </SliderContainer>
+            <Swiper
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 5000 }}
+                loop
+                >
+                <SwiperSlide><a href="#"><img src={slidero} alt="slider"/></a></SwiperSlide>
+                <SwiperSlide><a href="#"><img src={slidert} alt="slider"/></a></SwiperSlide>
+                <SwiperSlide><a href="#"><img src={sliderh} alt="slider"/></a></SwiperSlide>
+                <SwiperSlide><a href="#"><img src={sliderf} alt="slider"/></a></SwiperSlide>
+
+            </Swiper>
         )
     }
 }
